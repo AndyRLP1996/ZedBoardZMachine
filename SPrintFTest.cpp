73967f1bc50849d91f3cfecb879343434c4e23f6
AndyRLP1996 <31877629+AndyRLP1996@users.noreply.github.com>
2018-08-06 17:56:43 -0700

    updated sprintf test file to demonstrate pointer vs raw array.

--- SPrintFTest.cpp
+++ SPrintFTest.cpp
@@ -9,8 +9,13 @@ using namespace std;
 
 int main()
 {
+    char* temp;
+    temp=new char[10];
     char buffer[10];
     int x;
-    x=sprintf(buffer,"testing...");
+    x=sprintf(buffer,"testing123");
+    x=sprintf(temp,"testing...");
+    cout<<x<<endl;
     cout<<buffer<<endl;
+    cout<<temp<<endl;
 }
